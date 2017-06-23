a4584fa1b4e2d8292120cdd5bb648f3545280f07
Mykaelos <mmaderii@gmail.com>
2017-06-23 07:29:49 -0500

    Cleaned up the code, removed unused methods, suppressed warnings that don't matter. Created a buildJar.bat so that you only have to double click it to make a build. There's probably a far better way to do this, but good enough for now. Committing the jar itself so that anyone can just grab it without having to actually build the project.

--- app/src/main/java/com/mykaelos/unityandroidvibrator/Vibrator.java
+++ app/src/main/java/com/mykaelos/unityandroidvibrator/Vibrator.java
@@ -1,15 +1,14 @@
 package com.mykaelos.unityandroidvibrator;
 
 
-import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
-import android.widget.Toast;
 
+@SuppressWarnings({"StaticFieldLeak", "WeakerAccess", "unused"})
 public class Vibrator {
     private static final String TAG = Vibrator.class.getSimpleName();
+
     private static Vibrator instance;
-    private Context context;
     private android.os.Vibrator vibratorService;
 
     private Vibrator() {
@@ -24,34 +23,27 @@ public class Vibrator {
     }
 
     public void setContext(Context context) {
-        Log.i(TAG, "setContext");
-        this.context = context;
+        Log.d(TAG, "setContext");
         vibratorService = (android.os.Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     public boolean hasVibrator() {
+        Log.d(TAG, "hasVibrator: " + vibratorService.hasVibrator());
         return vibratorService.hasVibrator();
     }
 
     public void vibrate(long duration) {
-        Log.i(TAG, "vibrate for " + duration);
+        Log.d(TAG, "vibrate for " + duration);
         vibratorService.vibrate(duration);
     }
 
     public void vibrate(long[] pattern, int repeat) {
+        Log.d(TAG, "vibrate repeat: " + repeat);
         vibratorService.vibrate(pattern, repeat);
     }
 
-    public void toast(String message){
-        Log.i(TAG, "toast for " + message);
-
-        final String tempString = message;
-
-        ((Activity)context).runOnUiThread(new Runnable() {
-            public void run() {
-                Log.i(TAG, "Toast.makeText");
-                Toast.makeText(context, tempString, Toast.LENGTH_LONG).show();
-            }
-        });
+    public void cancel() {
+        Log.d(TAG, "vibrate cancel");
+        vibratorService.cancel();
     }
 }
